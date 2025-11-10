a03c7714e29e4b43d2919de2612fd34a41b13222
Alijon Ergashev <info@1week.tj>
2025-11-10 11:00:51 +0500

    Remove yandex_mapkit, switch to flutter_map

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import UIKit
 import Flutter
-import YandexMapsMobile 
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,9 +7,6 @@ import YandexMapsMobile
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // ⬇️ ИН САТРРО ИЛОВА КУНЕД ⬇️
-    YMKMapKit.setApiKey("d5e38efa-ec24-409f-9336-81be148be796")
-
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
